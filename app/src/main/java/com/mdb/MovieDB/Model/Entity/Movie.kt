51ba6954efc3e3ec0da580b38c96f4ffc6b2f9ca
rajibhossainpavel <rajibh.pavel@gmail.com>
2022-05-21 21:56:41 +0600

    working on model branch!

--- app/src/main/java/com/mdb/MovieDB/Model/Entity/Movie.kt
+++ app/src/main/java/com/mdb/MovieDB/Model/Entity/Movie.kt
@@ -1,21 +1,21 @@
 package com.mdb.MovieDB.Model.Entity
 
 class Movie {
-    val page: integer,
-    val poster_path: string?,
-    val adult: boolean,
-    val overview: string,
-    val release_date: string,
-    val genre_ids: array[integer],
-    val id: integer,
-    val original_title: string,
-    val original_language: string,
-    val title: string,
-    val backdrop_path: string?,
-    val popularity :number,
-    val vote_count: integer,
-    val video : boolean,
-    val vote_average :number,
-    val total_results: integer,
-    val total_pages: integer
+    val page: Int,
+    val poster_path: String?,
+    val adult: Boolean,
+    val overview: String,
+    val release_date: String,
+    val genre_ids:  ArrayList(),
+    val id: Int,
+    val original_title: String,
+    val original_language: String,
+    val title: String,
+    val backdrop_path: String?,
+    val popularity : Double,
+    val vote_count: Int,
+    val video : Boolean,
+    val vote_average :Double,
+    val total_results: Int,
+    val total_pages: Int
 }
