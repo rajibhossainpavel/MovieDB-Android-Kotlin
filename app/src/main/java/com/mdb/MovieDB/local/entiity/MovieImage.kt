ce361e1778f389bc9cab660100e58cb7f5113150
rajibhossainpavel <rajibh.pavel@gmail.com>
2022-05-22 08:40:35 +0600

    new!

--- app/src/main/java/com/mdb/MovieDB/local/entiity/MovieImage.kt
+++ app/src/main/java/com/mdb/MovieDB/local/entiity/MovieImage.kt
@@ -31,3 +31,31 @@ data class Poster(
     val vote_count: Int,
     val width: Int
 )
+
+/**
+{
+  "id": 550,
+  "backdrops": [
+    {
+      "aspect_ratio": 1.77777777777778,
+      "file_path": "/fCayJrkfRaCRCTh8GqN30f8oyQF.jpg",
+      "height": 720,
+      "iso_639_1": null,
+      "vote_average": 0,
+      "vote_count": 0,
+      "width": 1280
+    }
+  ],
+  "posters": [
+    {
+      "aspect_ratio": 0.666666666666667,
+      "file_path": "/fpemzjF623QVTe98pCVlwwtFC5N.jpg",
+      "height": 1800,
+      "iso_639_1": "en",
+      "vote_average": 0,
+      "vote_count": 0,
+      "width": 1200
+    }
+  ]
+}
+ */
