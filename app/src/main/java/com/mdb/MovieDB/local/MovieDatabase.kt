01791b5269e0470f463e33a9f428dd9eaec5dab8
rajibhossainpavel <rajibh.pavel@gmail.com>
2022-05-22 10:48:06 +0600

    new!

---
+++ app/src/main/java/com/mdb/MovieDB/local/MovieDatabase.kt
@@ -0,0 +1,17 @@
+package com.mdb.movieDB.local
+
+import androidx.room.Database
+import com.mdb.movieDB.local.dao.MovieDao
+import com.mdb.movieDB.local.dao.MovieDetailDao
+import com.mdb.movieDB.local.dao.MovieImageDao
+import com.mdb.movieDB.local.entiity.Movie
+import com.mdb.movieDB.local.entiity.MovieDetail
+import com.mdb.movieDB.local.entiity.MovieImage
+
+
+@Database(entities = [Movie::class, MovieDetail::class,  MovieImage::class], version = 1)
+    abstract class AppDatabase : RoomDatabase() {
+        abstract fun MovieDao(): MovieDao
+        abstract fun MovieDetailDao(): MovieDetailDao
+        abstract fun MovieImageDao(): MovieImageDao
+    }
